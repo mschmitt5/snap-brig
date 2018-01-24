a891b8ed9ed9629312d0c230c8b9cf448f026eeb
Mary <schmitt.mary7@gmail.com>
2018-01-24 08:31:10 -0700

    updated with everything but specific array data echoing

---
+++ CrimeArray.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: schmi
+ * Date: 1/24/2018
+ * Time: 8:08 AM
+ **/
+
+
+
+$prisonerCrime = ["larceny", "embezzlement", "praising Comcast", "tampering with replicators", "vehicular manslaughter"];
+
+$prisonerName = ["Brent", "Miguel", "Nat"];
+
+$prisoner1 = array_rand($prisonerName);
+$prisoner1Crime = array_rand($prisonerCrime);
+
+echo "$prisoner1 charged with $prisoner1Crime";
